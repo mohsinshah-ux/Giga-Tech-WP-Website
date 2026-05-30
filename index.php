88eb3400e9ee917be8fbbef6ade838b1cda5afff
Syed Mohsin Ali <65657466+mohsinshah-ux@users.noreply.github.com>
2026-05-31 02:00:06 +0500

    Backup: Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,17 @@
 <?php
-// Simulated index.php for 2026-05-30_20-11-42
+/**
+ * Front to the WordPress application. This file doesn't do anything, but loads
+ * wp-blog-header.php which does and tells WordPress to load the theme.
+ *
+ * @package WordPress
+ */
+
+/**
+ * Tells WordPress to load the WordPress theme and output it.
+ *
+ * @var bool
+ */
+define( 'WP_USE_THEMES', true );
+
+/** Loads the WordPress Environment and Template */
+require __DIR__ . '/wp-blog-header.php';
